a0e99b5dc27e13ffba52c58eae90fb020223ebf8
Jones Nunes <jonesnusan@gmail.com>
2021-07-29 12:43:20 -0300

    removenpasta backup

--- backup/include/helloWorld.h
+++
@@ -1,6 +0,0 @@
-#ifndef HELLOWORLD_H_INCLUDED
-#define HELLOWORLD_H_INCLUDED
-
-void helloWorld(void);
-
-#endif
--- backup/src/helloWorld.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "../include/helloWorld.h"
-
-void helloWorld(void){
-	printf("Hello World!\n");
-}
